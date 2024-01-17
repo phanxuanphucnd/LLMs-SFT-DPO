0e4efa81672fa6b2e958e3183e8082a834ab967c
EC2 Default User <ec2-user@ip-172-31-15-89.ap-northeast-1.compute.internal>
2024-01-17 19:27:56 +0000

    init

---
+++ run.sh
@@ -0,0 +1,20 @@
+CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
+    --stage sft \
+    --do_train \
+    --model_name_or_path jan-hq/Vistral-7B-Chat-DPO \
+    --dataset alpaca_gpt4_en \
+    --template 'llama2' \
+    --finetuning_type lora \
+    --lora_target q_proj,v_proj \
+    --output_dir ./models/Vistral-7B-Chat-DPO \
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --plot_loss \
+    --fp16
