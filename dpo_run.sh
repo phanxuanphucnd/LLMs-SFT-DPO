4cd0362e9a69f0444234b4db6391b59563aa2065
Ubuntu <ubuntu@ip-172-31-63-214.ec2.internal>
2024-01-31 10:29:39 +0000

    update

--- dpo_run.sh
+++ dpo_run.sh
@@ -1,25 +1,22 @@
 deepspeed --num_gpus 4 --master_port=9901 src/train_bash.py \
     --deepspeed configs/ds_config.json \
-    --stage sft \
+    --stage dpo \
     --do_train \
     --model_name_or_path Viet-Mistral/Vistral-7B-Chat \
-    --dataset misa_amiskt_labeled_vi \
+    --adapter_name_or_path models/AMIS/misa-vistral-7b-chat-use_unsloth-lora-3ep-lr5e5-r32-alpha16 \
+    --create_new_adapter \
+    --dataset comparison_amiskt_vi \
     --template 'llama2' \
     --finetuning_type lora \
     --lora_target all \
-    --output_dir models/AMIS/misa-vistral-7b-dpo-lora-4ep-lr5e5-r16-alpha16 \
-    --overwrite_cache \
-    --overwrite_output_dir \
+    --output_dir models/DPO/misa-vistral-7b-chat-use_unsloth-lora-3ep-lr5e5-r32-alpha16-DPO \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 2 \
     --lr_scheduler_type cosine \
-    --cutoff_len 4000 \
     --logging_steps 20 \
     --save_steps 1000 \
-    --learning_rate 5e-5 \
-    --num_train_epochs 4 \
+    --learning_rate 1e-5 \
+    --num_train_epochs 3 \
     --plot_loss \
-    --lora_rank 16 \
-    --lora_alpha 16 \
     --fp16 \
     --use_unsloth
